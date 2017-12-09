1b4c8c64d51265513ffa90629800f6912795ff4b
Enio Fernandes <ef@cic.ac>
2017-12-09 11:14:51 +0000

    trim imports

--- predict.py
+++ predict.py
@@ -1,21 +1,16 @@
+from tensorflow.python.keras.preprocessing import image
+from tensorflow.python.keras.applications.inception_v3 import preprocess_input, decode_predictions
 import numpy as np
-import tensorflow as tf
-
-from keras.applications.imagenet_utils import decode_predictions
-from keras.preprocessing import image
-from imageio import imread
-from nasnet import mobile, preprocess
 import nasnet
 
-image = imread('image.jpg')
+img = image.load_img('image.jpg', target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
 
 model = nasnet.mobile()
 model.load_weights('mobile.h5')
 
-size = model.input_shape[2]
-x = np.expand_dims(preprocess(image, size=size), axis=0)
-
 y = model.predict(x)
-
 for index, res in enumerate(decode_predictions(y)[0]):
     print('{}. {}: {:.3f}%'.format(index + 1, res[1], 100 * res[2]))
